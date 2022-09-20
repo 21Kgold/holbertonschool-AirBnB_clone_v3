26353995618de87474d632c13e4bf6a7ea575acb
21Kgold <claudiarpa@gmail.com>
2022-09-20 16:35:52 -0700

    5. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-index.py file returns status of API
+index.py file returns status and stats of API
 """
 
 from api.v1.views import app_views
 from flask import jsonify
 # import json
+from models import storage
 
 
 @app_views.route('/status')
@@ -19,5 +20,18 @@ def status():
     # responses. Whereas, the json.dumps() method will just return an encoded
     # string, which would require manually adding the MIME type header.
     # json_string = json.dumps(python_dict)
+    flask_response = jsonify(python_dict)
+    return flask_response
+
+
+@app_views.route('/stats')
+def stats():
+    """
+    Returns the number of all objects by type class as a flask response
+    """
+    python_dict = {"amenities": storage.count("Amenity"), "cities": storage.
+                   count("City"), "places": storage.count("Place"), "reviews":
+                   storage.count("Review"), "states": storage.count("State"),
+                   "users": storage.count("User")}
     flask_response = jsonify(python_dict)
     return flask_response
