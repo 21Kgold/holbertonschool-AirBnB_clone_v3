5f0893632c84d6db46abc4f5deb560cb2e7dada3
21Kgold <claudiarpa@gmail.com>
2022-09-20 17:17:25 -0700

    6. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,20 @@
 Create variable app as instance of flask
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    """
+    Custom message for 404 error
+    """
+    return (jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
